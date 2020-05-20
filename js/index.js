33227f1b147ac9f4d1cad12a06f761896bbbb167
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-20 14:02:21 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
 
 
 
